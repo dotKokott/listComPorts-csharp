588564410e0f2c65f496ed577e7ea7292bcdb551
Christian Kokott <christian.kokott@gmail.com>
2019-10-21 16:53:23 +0200

    Initial version

---
+++ listComPorts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Management;
+using System.Text.RegularExpressions;
+
+namespace listComPorts {
+    class Program {
+        static void Main(string[] args) {
+            var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity");
+            Regex comRegex = new Regex(@"COM[0-9]+");
+
+            foreach(var obj in searcher.Get()) {
+                var name = (string)obj["Name"];
+                if (name == null) continue;
+
+                var match = comRegex.Match(name);
+                if(match.Success) {
+                    Console.WriteLine(match.Value + " " + (string)obj["DeviceID"]);
+                }   
+            }     
+        }
+    }
+}
