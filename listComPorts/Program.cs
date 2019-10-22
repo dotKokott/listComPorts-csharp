665ec4c5dd4f174bf2ed1b9c2658e5fc59da8e7c
Christian Kokott <christian.kokott@gmail.com>
2019-10-22 13:07:30 +0200

    added usage & search

--- listComPorts/Program.cs
+++ listComPorts/Program.cs
@@ -4,7 +4,25 @@ using System.Text.RegularExpressions;
 
 namespace listComPorts {
     class Program {
+        static void showUsage() {
+            Console.WriteLine("Usage: listComPorts [-h] [-s <search>]");
+            Console.WriteLine("\t-s <search> : Search by any string contained in device description");
+            Console.WriteLine("\t-h : This help message");
+
+            Environment.Exit(1);
+        }
+
         static void Main(string[] args) {
+            var searchString = "";
+            for(int i = 0; i < args.Length; i++) {
+                var arg = args[i];
+                if(arg.Equals("-h")) {
+                    showUsage();
+                } else if(arg.Equals("-s")) {
+                    searchString = args[++i];
+                }
+            }
+
             var query = @"SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%(COM%'";
             
             var comRegex = new Regex(@"COM[0-9]+");
@@ -13,6 +31,10 @@ namespace listComPorts {
             foreach (var obj in searcher.Get()) {
                 var name = (string)obj["Name"];
                 if (name == null) continue;
+
+                //TODO: Move this into the query
+                var deviceID = (string)obj["DeviceID"];
+                if (!string.IsNullOrEmpty(searchString) && !deviceID.Contains(searchString)) continue;
 
                 var match = comRegex.Match(name);
                 if(match.Success) {
